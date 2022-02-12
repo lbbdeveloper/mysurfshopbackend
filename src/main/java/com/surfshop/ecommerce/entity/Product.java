67d1672eac2029f71984f2dc29f8dab5c950960a
a <96930550+lbbdeveloper@users.noreply.github.com>
2022-02-12 10:56:36 -0500

    git initial

---
+++ src/main/java/com/surfshop/ecommerce/entity/Product.java
@@ -0,0 +1,37 @@
+package com.surfshop.ecommerce.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="product")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private ProductCategory category;
+
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+
+
+}
